4de7c4c084c7ccf65228abd1ced9db9c3d3f39f0
Stéphane Veyret <stephane@neptura.org>
2022-09-10 17:24:13 +0200

    Update dependencies

--- src/transform/transform.ts
+++ src/transform/transform.ts
@@ -14,16 +14,15 @@ type TransformerMetaFactory = (program: Program, configuration: unknown) => Tran
 
 export default function <C extends NodeVisitorContext>(
   Context: NodeVisitorContextType<C>,
-  NodeVisitors: NodeVisitorType<C>[]
+  NodeVisitors: Array<NodeVisitorType<C>>
 ): TransformerMetaFactory {
   return (program: Program, configuration: unknown): TransformerFactory<SourceFile> => {
     const context: C = new Context(program, configuration)
-    return (transContext: TransformationContext): Transformer<SourceFile> => (
-      sourceFile: SourceFile
-    ): SourceFile => {
-      context.initNewFile(transContext, sourceFile)
-      const nodeVisitors = NodeVisitors.map(NodeVisitor => new NodeVisitor(context))
-      return visitNode(sourceFile, buildVisitor(transContext, nodeVisitors))
-    }
+    return (transContext: TransformationContext): Transformer<SourceFile> =>
+      (sourceFile: SourceFile): SourceFile => {
+        context.initNewFile(transContext, sourceFile)
+        const nodeVisitors = NodeVisitors.map(NodeVisitor => new NodeVisitor(context))
+        return visitNode(sourceFile, buildVisitor(transContext, nodeVisitors))
+      }
   }
 }
